1dbba7c17ccc834f8ba3288530093ec872fcd58d
Lucas Cardoso <lccao@cin.ufpe.br>
2019-09-26 15:54:24 -0300

    Initial setup commit

---
+++ biscoitometro/biscoitometro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  biscoitometro
+//
+//  Created by Lucas Cardoso on 26/09/19.
+//  Copyright © 2019 Lucas Cardoso. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ biscoitometro/biscoitometroTests/biscoitometroTests.swift
@@ -0,0 +1,34 @@
+//
+//  biscoitometroTests.swift
+//  biscoitometroTests
+//
+//  Created by Lucas Cardoso on 26/09/19.
+//  Copyright © 2019 Lucas Cardoso. All rights reserved.
+//
+
+import XCTest
+@testable import biscoitometro
+
+class biscoitometroTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
